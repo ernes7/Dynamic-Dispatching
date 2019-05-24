e7af97b36922fd628c475ddc59a5358791ad5a66
Ernesto Martinez <ernechelsea@gmail.com>
2019-05-23 23:13:31 -0700

    lab finished

--- list.py
+++ list.py
@@ -8,6 +8,16 @@ class Nil:
       return other
     def __add__(self,other):  #STEP 5#
       return other
+    def map(self,f):  #STEP 6#
+        return self
+    def filter(self, pred): #STEP 6#
+        return self
+    def lengthAcc(self, a):      #STEP 7#
+        return a
+    def sumAcc(self, acc): #STEP 7#
+        return acc
+    def rev0nto(self, list): #STEP 7#
+        return list
 
 class Cons:
     """ Represents a non-empty list. """
@@ -22,6 +32,17 @@ class Cons:
         return Cons(self.head, self.tail.append(other))
     def __add__(self, other):  #STEP 5#
         return Cons(self.head, self.tail + other)
+    def map(self,f):  #STEP 6#
+        return Cons(f(self.head), self.tail.map(f))
+    def filter(self, pred): #STEP 6#
+        rest = self.tail.filter(pred)
+        return Cons(self.head, rest) if pred(self.head) else rest
+    def lengthAcc(self, a):      #STEP 7#
+        return self.tail.lengthAcc(1+a)
+    def sumAcc(self, acc): #STEP 7#
+        return self.tail.sumAcc(acc + self.head)
+    def rev0nto(self, list): #STEP 7#
+        return self.tail.rev0nto(Cons(self.head, list))
 
 
 alist = Cons(1, Cons(2, Cons(3, Nil())))
@@ -37,11 +58,14 @@ def powers2(n):
     return loop(1,n)
 
 """ Testing """
+# 7 #
+#print(alist.sumAcc(0))
+
 # 5 #
-print(alist + blist)
-print(blist + alist)
-print((alist + blist).length())
-print(alist + blist.length())
+#print(alist + blist)
+#print(blist + alist)
+#print((alist + blist).length())
+#print(alist + blist.length())
 
 # 1 & 2 #
 #print(alist)
